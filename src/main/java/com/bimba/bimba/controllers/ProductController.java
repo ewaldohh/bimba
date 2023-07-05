1839a5e5e7d7e492e388c82d32e535121e35eabb
unknown <ewaldohh>
2023-07-05 13:51:23 +0700

    Third Commit

--- src/main/java/com/bimba/bimba/controllers/ProductController.java
+++ src/main/java/com/bimba/bimba/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.bimba.bimba.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bimba.bimba.dto.ResponseData;
 import com.bimba.bimba.models.entities.Product;
 import com.bimba.bimba.services.ProductService;
 
@@ -25,15 +28,20 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping
-    public Product create(@Valid @RequestBody Product product, Errors errors){
+    public ResponseEntity<ResponseData<Product>> create(@Valid @RequestBody Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<>();
 
         if (errors.hasErrors()){
             for (ObjectError error : errors.getAllErrors()) {
-                System.err.println(error.getDefaultMessage());
+                responseData.getMessages().add(error.getDefaultMessage());
             }
-            throw new RuntimeException("Validation Error");
+                responseData.setStatus(false);
+                responseData.setPayload(null);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
-        return productService.save(product);
+        responseData.setStatus(true);
+        responseData.setPayload(productService.save(product));
+        return ResponseEntity.ok(responseData);
     }
 
     @GetMapping
@@ -47,8 +55,20 @@ public class ProductController {
     }
 
     @PutMapping
-    public Product update(@RequestBody Product product){
-        return productService.save(product);
+    public ResponseEntity<ResponseData<Product>> update(@Valid @RequestBody Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<>();
+
+        if (errors.hasErrors()){
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessages().add(error.getDefaultMessage());
+            }
+                responseData.setStatus(false);
+                responseData.setPayload(null);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.save(product));
+        return ResponseEntity.ok(responseData);
     }
 
     @DeleteMapping("/{id}")
